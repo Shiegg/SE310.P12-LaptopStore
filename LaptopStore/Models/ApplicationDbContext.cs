c14a3d12a00c844cbd3e44261820afabcd13d2f5
Shieng <22521312@gm.uit.edu.vn>
2024-10-28 14:55:52 +0700

    final

---
+++ LaptopStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LaptopStore.Models
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+		public DbSet<Product> Products { get; set; }
+	}
+
+}
